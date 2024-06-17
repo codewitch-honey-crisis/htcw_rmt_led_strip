a827172360c6a5d6c85a41c9b9fccdebfc0a0f9d
codewitch-honey-crisis <gaztoast@gmail.com>
2024-06-17 09:09:22 -0700

    added example

---
+++ examples/demo/src/main.cpp
@@ -0,0 +1,16 @@
+#include <Arduino.h>
+#include <rmt_led_strip.hpp>
+
+// 1 LED, pin 15
+arduino::ws2812 leds(15,1);
+
+void setup() {
+  Serial.begin(115200);
+  leds.initialize();
+  // set led index 0 to purple
+  leds.color(0,255,0,255);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
